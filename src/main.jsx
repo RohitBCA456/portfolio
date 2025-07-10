f0666e2d3583cb5d4bd3b5af9e00d5e8e49b397e
RohitBCA456 <ry2281104@gmail.com>
2025-07-10 17:59:02 +0530

    fix: Update main.jsx to wrap RouterProvider in BrowserRouter for proper routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </BrowserRouter>
 );
